4d1d24fbfca5eba3e9da549cb08e2444e9491991
subasskhatrii <subash@teksewa.com>
2022-05-04 21:31:53 +0545

    Refactor Controller - Create a separate class to scrape inforamtion

--- app/controllers/scrape_informations_controller.rb
+++ app/controllers/scrape_informations_controller.rb
@@ -1,7 +1,3 @@
-require 'selenium-webdriver'
-require 'nokogiri'
-require 'httparty'
-
 class ScrapeInformationsController < ApplicationController
   def index
     render json: { data: scrape_information_list.to_json }
@@ -9,31 +5,20 @@ class ScrapeInformationsController < ApplicationController
 
   # TODO: Need to refactor this into a service class
   def scrape_information_from_api
-    @url = 'https://hamrobazaar.com/mobile-phone-handsets/apple-iphone-iphone-11-pro-max-256-gold-in-kathmandu/1a8657be4c084b33b65dd264fa9361d5'
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.headless!
-    driver = Selenium::WebDriver.for(:chrome, capabilities: [options])
-    driver.get @url
-    response = Nokogiri::HTML(driver.page_source)
-    page_details = response.css('.page--details')
-    page_details.each do |page_detail|
-      @title = page_detail.css('.page--title').css('h3').text
-      @description = page_detail.css('.ad--informations').css('.ad--desc').css('p').text
+    url = 'https://hamrobazaar.com/mobile-phone-handsets/apple-iphone-iphone-11-pro-max-256-gold-in-kathmandu/1a8657be4c084b33b65dd264fa9361d5'
+    scrape_results = WebScraper.call(url)
+    scrape_infos = ScrapeInfo.create!(scrape_results)
+    if scrape_infos.save
+      render json: { data: scrape_infos, success: true, message: 'Scraped Successfully!' },
+             status: :ok
+    else
+      render status: 401, json: {
+        success: false,
+        message: 'Scraped Failure!'
+      }
     end
-
-    page_details_asides = response.css('.page--details--aside')
-    page_details_asides.each do |aside|
-      @price = aside.css('h2').text
-      @phone_number = aside.css('.seller__infos').css('.seller__address').css('span').text
-    end
-    scrape_informations = {
-      url: @url,
-      title: @title,
-      description: @description,
-      price: @price,
-      phone_number: @phone_number
-    }
-    @scrape_info = ScrapeInfo.create!(scrape_informations)
+  rescue StandardError => e
+    puts e.to_s
   end
 
   private
